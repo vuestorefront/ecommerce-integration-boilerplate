e446b954c5d933cd634da8318c6b5afb7d6da5d1
Mateusz Pietrusinski <1325258@gmail.com>
2021-10-06 11:40:37 +0200

    chore: temporary name change

--- packages/theme/middleware.config.js
+++ packages/theme/middleware.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   integrations: {
-    <% INTEGRATION %>: {
-      location: '@vue-storefront/<% INTEGRATION %>-api/server',
+    temp: {
+      location: '@vue-storefront/temp-api/server',
       configuration: {}
     }
   }
