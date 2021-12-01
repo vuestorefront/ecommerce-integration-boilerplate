fa0ef244c5a4c80c2413b337dedd578b48c52649
Filip Sobol <sobol.filip@gmail.com>
2021-12-01 17:31:35 +0100

    chore: remove extra whitespaces

--- packages/composables/src/getters/storeGetters.ts
+++ packages/composables/src/getters/storeGetters.ts
@@ -3,12 +3,12 @@ import { AgnosticStore, UseStoreGetters } from '@vue-storefront/core';
 import { UseStoreFilterParams } from '../types';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-function getItems (stores: Store, criteria: UseStoreFilterParams = {}): AgnosticStore[] {
+function getItems(stores: Store, criteria: UseStoreFilterParams = {}): AgnosticStore[] {
   return [];
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-function getSelected (stores: Store): AgnosticStore | undefined {
+function getSelected(stores: Store): AgnosticStore | undefined {
   return null;
 }
 
