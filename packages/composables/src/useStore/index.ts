d5e04ee62c75437cd9668f693e51ed96896a479a
Filip Sobol <sobol.filip@gmail.com>
2021-12-01 16:57:06 +0100

    fix: import in useStore composable

--- packages/composables/src/useStore/index.ts
+++ packages/composables/src/useStore/index.ts
@@ -1,4 +1,4 @@
-import { Store } from '@vue-storefront/boilerplate-api';
+import { Store } from '@vue-storefront/<% INTEGRATION %>-api';
 import { Context, useStoreFactory } from '@vue-storefront/core';
 
 export const useStore = useStoreFactory<Store>({
